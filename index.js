d00c12871be1126f4328cef6c94122a941f761b5
Mike240892 <Mike240892@users.noreply.github.com>
2020-05-02 16:38:22 +0200

    Project is finished

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
-const fs = require('fs');
+#!/usr/bin/env node
 
-fs.readdir(process.cwd(), (err, files)=>{
+const fs = require('fs');
+const util = require('util');
+const chalk = require('chalk');
+const path = require('path');
+
+// Method #2
+//const lstat = util.promisify(fs.lstat);
+
+// Method #3
+const { lstat } = fs.promises;
+
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames)=>{
     if(err){
         console.log(err);
     }
-    console.log(files);
+    
+    const statPromises = filenames.map(filename => {
+        return lstat(path.join(targetDir, filename));
+    });
+
+    const allStats = await Promise.all(statPromises);
+
+    for (const stats of allStats) {
+        const index = allStats.indexOf(stats);
+        if(stats.isFile()){
+            console.log(filenames[index]);
+        } else {
+            console.log(chalk.bold.red(filenames[index]));
+        }
+        
+    }
+
 });
+
+
+// Method #1
+/*
+const lstat = (filename) => {
+    return new Promise((resolve, reject) =>{
+        fs.lstat(filename, (err, stats)=>{
+            if(err){
+                reject(err);
+            }
+
+            resolve(stats);
+
+        })
+    })
+}
+*/
